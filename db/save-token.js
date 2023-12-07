54f40922c1a379bda367d1c025995e51b4a379b3
JeongSH1 <jyt001532@gmail.com>
2023-12-07 12:13:44 +0900

    chore: 내부 try-catch 삭제

--- db/save-token.js
+++ db/save-token.js
@@ -1,13 +1,6 @@
 import { connection } from "./connection.js";
 
 export async function saveToken(userId, token) {
-    try {
-      // SQL 쿼리
-      const [rows, fields] = await connection.execute('insert into tokens values(?, ?)', [userId, token]);
-        
-      return rows;
-      
-    } catch (err) {
-      console.error(err);
-    }
+    const [rows, fields] = await connection.execute('insert into tokens values(?, ?)', [userId, token]);
+    return rows;
   }
