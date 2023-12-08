c134bff5c1a125b0823a2bbb7a5d71c7f0789d47
JeongSH1 <jyt001532@gmail.com>
2023-12-08 12:59:03 +0900

    enhancement: update, save log 추가

--- db/save-token.js
+++ db/save-token.js
@@ -6,9 +6,12 @@ export async function saveToken(userId, token) {
 
     if (!token){
       const [rows, fields] = await connection.execute('insert into tokens values(?, ?)', [userId, token]);
+      console.log(`${new Date()}: ${userId} save Token`)
       return rows;
     } else {
       const [rows, fields] = await connection.execute('update tokens set token = ? where userId = ?', [token, userId]);
+      console.log(`${new Date()}: ${userId} update Token`)
       return rows;
     }
+
   }
